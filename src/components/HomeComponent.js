a0696bc94853692696a71fa29232ea864f8bcdd5
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2020-07-19 00:02:01 +0100

    modal sign-up

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label } from 'reactstrap';
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSignup = this.handleSignup.bind(this);
+        this.state = {
+          isModalOpen: false
+        };
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSignup(event) {
+      this.toggleModal();
+      alert("Username: " + this.username.value + " E-mail: " + this.remember.email
+        + " Password: " + this.password.value
+        );
+      event.preventDefault();
+    }
+
     render(){
         return(
             <div className='container'>
@@ -13,9 +39,37 @@ class Home extends Component {
                     <div>
                         <Button type="submit" value="submit" style={{marginRight: 20}} color="primary">Learn more</Button>
                         <span><span></span></span>
-                        <Button outline color="primary">Register</Button>
+                        <Button outline onClick={this.toggleModal} color="primary">Register</Button>
                     </div>
                 </div>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Sign Up</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleSignup}>
+                            <FormGroup>
+                                <Label htmlFor="username">Full Name</Label>
+                                <Input type="text" id="username" name="username"
+                                   innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="email">E-mail</Label>
+                                <Input type="text" id="email" name="email"
+                                   innerRef={(input) => this.email = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Confirm Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Sign Up</Button>
+                        </Form>
+                    </ModalBody>
+            </Modal>
             </div>
         );
     }
